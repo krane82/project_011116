f6b0dc0a81cc7bfe8d6bf8d29081e56fa750bac4
tonkoshkurik <tonkoshkurik@yandex.ua>
2017-03-07 18:56:38 +0200

    sql

--- sql.sql
+++ sql.sql
@@ -1,8 +1,6 @@
 
 /* ALTER TABLE `leads_delivery` ADD `postcode` VARCHAR(5) NULL DEFAULT NULL AFTER `timedate`; */
 
-CREATE TABLE IF NOT EXISTS lead_settings (days int(3));
-
 ALTER TABLE `clients_criteria` CHANGE `postcodes` `postcodes` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
 
 ALTER TABLE `leads_lead_fields_rel` CHANGE `note` `note` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;
@@ -34,3 +32,39 @@ ALTER TABLE `leads_rejection` ADD `decline_reason` VARCHAR(500) NULL DEFAULT NUL
 /* Key for post:
 9b434eba-e278-4522-a6ac-5ef14e1fb18d
 */
+
+/* last result
+
+ALTER TABLE `clients_criteria` CHANGE `postcodes` `postcodes` VARCHAR(500) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+ALTER TABLE `leads_lead_fields_rel` CHANGE `note` `note` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL;# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+
+CREATE TABLE IF NOT EXISTS `queue` (
+ `id` int(20) NOT NULL,
+ `id_lead` int(20) NOT NULL,
+ `id_client` int(20) NOT NULL,
+ `timedata` int(10) NOT NULL,
+ `status` tinyint(20) NOT NULL,
+ `test` varchar(20) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=134 DEFAULT CHARSET=utf8;# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+ALTER TABLE `queue`
+ ADD PRIMARY KEY (`id`);# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+ALTER TABLE `queue`
+ MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=134;# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+CREATE TABLE IF NOT EXISTS lead_settings (days int(3));# MySQL вернула пустой результат (т.е. ноль строк).
+
+
+ALTER TABLE `leads_delivery` ADD `open_time` INT(10) UNSIGNED NULL DEFAULT NULL AFTER `open_email`;# Затронуто 3732 строки.
+
+ALTER TABLE `leads_rejection` ADD `decline_reason` VARCHAR(500) NULL DEFAULT NULL AFTER `note`;# Затронуто 3722 строки.
+
+ */
