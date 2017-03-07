8e516ed54bd5136593894a840c23c4cd9336e124
viktor <krane82@gmail.com>
2017-03-07 18:49:50 +0200

    Cron manager finished, cron tasks 1 & 2 added

--- app/classes/cron.php
+++ app/classes/cron.php
@@ -4,10 +4,19 @@ include "../libs/PHPMailer/class.phpmailer.php";
 include "../core/model.php";
 include "../models/model_api.php";
 include "../core/controller.php";
-class cron extends Controller    {
+class cron   {
 
     function __construct() {
         $this->model = new Model_Api();
+    }
+    public function db()
+    {
+        $con = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+        if ($con->connect_errno) {
+            printf("Connect failed: %s\n", $con->connect_error);
+            exit();
+        }
+        return $con;
     }
     public function action_index() {
         $leads=$this->model->getSentLeads();
--- app/cron/crontask.php
+++ app/cron/crontask.php
@@ -1,6 +1,7 @@
 <?php
 include "../../config.php";
 include "../classes/cron.php";
+include "../classes/cron1.php";
 include "../core/functions.php";
 //Here we add cron tasks and check for time to run it
 //This method will run cron task for resend leads that does not have 4 matches
@@ -8,4 +9,10 @@ if (date('wGi')=='21201')
 {
     $resent = new cron();
     $resent->action_index();
+}
+//Here is Arthur's cron task
+if (date('i')=='01')
+{
+    $cron1=new cron1();
+$cron1->getleads();
 }
