d2debc0becac1b48f7ba60bac74663d2114fe295
viktor <krane82@gmail.com>
2017-03-23 19:44:54 +0200

    Updated model_leads & controller_leads

--- app/models/model_leads.php
+++ app/models/model_leads.php
@@ -32,16 +32,15 @@ class Model_Leads extends Model {
     $con = $this->db();
     $sql = "SELECT * FROM leads_lead_fields_rel WHERE id='".$id."'";
     $res = $con->query($sql);
-    if($res){
-      $r = $res->fetch_assoc();
-      return $r;
-    } else
-      return "Lead not found";
+    if($res) {
+      if ($r = $res->fetch_assoc()) return $r;
+      return false;
+    }
   }
   private function getClientById($id)
   {
     $con = $this->db();
-    $sql = 'SELECT cc.id, c.email, c.full_name';
+    $sql = 'SELECT cc.id, c.email, c.full_name, cc.postcodes';
     $sql.= ' FROM `clients_criteria` as cc';
     $sql.= ' LEFT JOIN `clients` as c ON cc.id = c.id';
     $sql.= " WHERE cc.id=$id";
@@ -54,33 +53,62 @@ class Model_Leads extends Model {
     }
     return $client;
   }
-
-  public function senLead($client_id, $lead_id)
+  public function senManyLeads($client_id, $start, $end, $state, $source)
   {
-    //var_dump($client_id);
-    $receivers=$this->getLeadFromDelivered($lead_id);
-    //This can be optionally commented
-    //Check when you try to resend lead
-    //to current client if this lead is already
-    //sent 4 or more times
-    $counter = count($receivers);
-    if($counter>=4)
-    {
-      return 'This lead already sent 4 times';
-    }
-    //End of check if send 4 or more times for current client
-    if($client_id != 0){
-      if(in_array($client_id, $receivers ))
-      {
-        return "This client already has this lead";
+    $data=$this->getList($start, $end, $state, $source);
+    $i=0;
+    if($client_id!=0) {
+      $c = $this->getClientById($client_id);
+      foreach ($data as $lead_id) {
+        if ($x = $this->senLeadToCurrent($client_id, $lead_id,$c)) print $x . '<br>';
+        else {
+          print 'Error!';
+        }
+        $i++;
+        usleep(250000);
       }
-      $leadInfo = $this->getLeadInfo($lead_id);
+    }
+    else {
+      foreach ($data as $lead_id) {
+        if ($x = $this->senLeadToAll($lead_id)
+        ) print $x . '<br>';
+        else {
+          print 'error!';
+        }
+        $i++;
+        usleep(250000);
+      }
+    }
+    print $i.' Leads done';
+  }
+  public function senOneLead($client_id,$lead_id)
+  {
+    if($client_id!=0) {
+      $c = $this->getClientById($client_id);
+      if ($x = $this->senLeadToCurrent($client_id, $lead_id, $c)) print $x;
+      else {
+        print 'Error!';
+      }
+    } else {
+      if ($x = $this->senLeadToAll($lead_id)) print $x;
+      else {
+        print 'Error!';
+      }
+    }
+  }
+  public function senLeadToCurrent($client_id, $lead_id, $c)
+  {
+    $receivers=$this->getLeadFromDelivered($lead_id);
+    $counter = count($receivers);
+    if($counter>=4) return 'This lead already sent 4 times';
+    $leadInfo = $this->getLeadInfo($lead_id);
+    $postcodes=explode(',',$c['postcodes']);
+    if (!in_array($leadInfo['postcode'],$postcodes)) return 'This client is unmatched to receive this lead';
+    if(in_array($client_id, $receivers )) return "This client already has this lead";
       $readyLeadInfo = prepareLeadInfo($leadInfo);
       $passedCaps = $this->api->checkClientsLimits($client_id);
-      $c = $this->getClientById($client_id);
       if($passedCaps) {
         $delivery_id = $this->getLastDeliveryID() + 1;
-        //var_dump($c);
         $sent = $this->sendToClient($c["email"], $readyLeadInfo, $c["full_name"],$delivery_id);
         if($sent) {
           $this->api->addToDeliveredTable($client_id, $lead_id, $readyLeadInfo);
@@ -91,17 +119,17 @@ class Model_Leads extends Model {
       } else {
         return "Cannot send over client caps...";
       }
-    }
-    if($client_id == 0)
-    {
-      $leadInfo = $this->getLeadInfo($lead_id);
-      $clients = $this->api->getClients($leadInfo);
-      //var_dump($clients);
-      //if (count($clients==0)) return 'No clients found for this lead';
-
-      $result = $this->sendToClients($clients, $lead_id, $leadInfo);
-      return $result;
-    }
+  }
+  public function senLeadToAll($lead_id)
+  {
+    $receivers=$this->getLeadFromDelivered($lead_id);
+    $counter = count($receivers);
+    if($counter>=4) return 'This lead already sent 4 times';
+    $leadInfo = $this->getLeadInfo($lead_id);
+    $state=$leadInfo['state'];
+    if(!$clients = $this->api->getClients($leadInfo)) return "No clients matches for this lead, or they are inactive";
+    $result = $this->sendToClients($clients, $lead_id, $leadInfo);
+    return $result;
   }
   private function getLeadFromDelivered($id)
   {
@@ -144,6 +172,8 @@ class Model_Leads extends Model {
     {
       return 'This lead already sent 4 times';
     }
+    $readyLeadInfo = prepareLeadInfo($p);
+    $delivery_id = $this->getLastDeliveryID();
     $sentTo = '';
     foreach ($clients as $c) {
       $id = $c["id"];
@@ -152,16 +182,18 @@ class Model_Leads extends Model {
         continue;
       }
       $passedCaps = $this->checkClientsLimits($id);
-      if($passedCaps AND $counter < 4) {
-        $readyLeadInfo = prepareLeadInfo($p);
-        $delivery_id = $this->getLastDeliveryID() + 1;
+      if($passedCaps) {
         $sent = $this->sendToClient($c["email"], $readyLeadInfo, $c["full_name"],$delivery_id);
         if($sent) {
           $counter++;
           $sentTo .= "Lead #$lead_id sent to $c[full_name] : $c[email]<br>\n";
           $this->api->addToDeliveredTable($id, $lead_id, $readyLeadInfo);
+          $delivery_id+=1;
+          return 'Lead sent and added to database';
         }
+        return 'for some reason lead can not be sent';
       }
+      return 'Out of clients caps';
     }
     return $sentTo;
   }
@@ -223,13 +255,7 @@ class Model_Leads extends Model {
       $sql = 'SELECT cc.id, c.email, c.full_name';
       $sql.= ' FROM `clients_criteria` as cc';
       $sql.= ' LEFT JOIN `clients` as c ON cc.id = c.id';
-      if(!empty($post["state"]) AND !empty($post["postcode"])) {
-        $sql .= ' WHERE cc.states_filter LIKE "%' . $post["state"] . '%" OR cc.postcodes LIKE "%'.$post["postcode"].'%"';
-      } else if(!empty($post["state"])) {
-        $sql .= ' WHERE cc.states_filter LIKE "%' . $post["state"] . '%"';
-      } else if(!empty($post["postcode"])){
-        $sql.= ' WHERE cc.postcodes LIKE "%'.$post["postcode"].'%"';
-      }
+      $sql .= ' WHERE cc.states_filter LIKE "%' . $post["state"] . '%" AND cc.postcodes LIKE "%'.$post["postcode"].'%"';
       $sql .= ' AND c.status = 1';
       $sql .= ' ORDER BY c.lead_cost DESC';
     } else {
