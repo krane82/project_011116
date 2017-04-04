1afbba50c5392df6c7d7b4c10bd2f7ae6c875a7b
artur_kardash <kardash1989@gmail.com>
2017-04-04 15:40:13 +0300

    report fix

--- app/models/model_admin_reports.php
+++ app/models/model_admin_reports.php
@@ -510,16 +510,16 @@ WHERE 1=1 AND (`l`.`datetime` BETWEEN 1488027600 AND 1488891600)";
       $result = $res->fetch_assoc();
     }
 
-   if($result['amount'] == 0){
+    if($result['amount'] == 0){
         $average = 0;
-   }else{
+    }else{
         $average = round($result['client_cost'] / $result['amount'], 2);
-   }
-      if($CountLids['amount'] == 0){
-            $average_sales = 0;
-      }else{
-            $average_sales = round($distributed["amount"] / $CountLids['amount'], 2);
-      }
+    }
+    if($CountLids['amount'] == 0){
+      $average_sales = 0;
+    }else{
+      $average_sales = round($distributed["amount"] / $CountLids['amount'], 2);
+    }
     if($distributed["amount"] == 0){
         $rejectedP = 0;
     }else{
@@ -535,7 +535,7 @@ WHERE 1=1 AND (`l`.`datetime` BETWEEN 1488027600 AND 1488891600)";
     $coastld = $CoastLids['cost']. " $<br>Total cost of<br> leads generated ";
     $totalcost = $resCoast. " $<br> Total income of<br>leads distributed ";
     $totalAverage = $average. " $<br>Average sales<br>per lead";
-    $av_sel = $average_sales . " Leads<br>Received";
+    $av_sel = $average_sales. " Leads<br>Received";
     echo $mes;
     echo $this->formStatView($ds, 'users', 'getDistributed');
     echo $this->formStatView($acs, 'check', 'getAccepted');
